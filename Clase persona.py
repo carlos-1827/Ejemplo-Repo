22f87210a87de295c846af1faaccff116afdd428
carlos-1827 <carlosjaviercastillolopes@gmail.com>
2025-04-13 11:44:35 -0600

    Agregue Clase Persona

---
+++ Clase persona.py
@@ -0,0 +1,13 @@
+class Persona:
+    def __init__(self, Nombres, Apellidos, Edad):
+        self.Nombres = Nombres
+        self.Apellidos = Apellidos
+        self.Edad = Edad
+
+    def infoPersona(self):
+        print(f'Los Nombres Son: {self.Nombres}')
+        print(f'Los Apellidos Son {self.Apellidos}')
+        print(f'La Edad es: {self.Edad} Años')
+
+persona1 = Persona("Carlos Javier", "Castillo Lopez", 20)
+persona1.infoPersona()
